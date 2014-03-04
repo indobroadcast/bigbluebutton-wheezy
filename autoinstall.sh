156862b9b370f2b1064fa70aedc96dc92a662cac
indobroadcast <admin@indobroadcast.com>
2014-03-04 09:46:10 +0700

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,3 +8,12 @@ wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
 
 # Add the BigBlueButton repository URL
 echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_081/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+
+# Install Flash plugin
+sudo aptitude -y -f install flashplugin-nonfree
+
+# Build-dep JAVA & PHP5 & Python & Ruby for ruby1.9.2_1.9.2-p290
+sudo aptitude -y -f build-dep php5
+sudo aptitude -y -f build-dep python
+sudo aptitude -y -f build-dep ruby
+sudo aptitude -y -f build-dep java-package
